3f878758cd6737e84e4af92183183d569e518d5b
FrancaTM <francatm@outlook.com>
2020-10-22 12:33:11 -0300

    add appbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import { styles } from "./css-common";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AddTutorial from "./components/add-tutorial.component";
+import Tutorial from "./components/tutorial.component";
+import TutorialsList from "./components/tutorials-list.component";
+
+import { AppBar, Toolbar, Typography, withStyles } from "@material-ui/core";
+
+class App extends Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div>
+        <AppBar className={classes.appBar} position="static">
+          <Toolbar>
+            <Typography className={classes.name} variant="h6">
+              SampleApp
+            </Typography>
+            <Link to={"/tutorials"} className={classes.link}>
+              <Typography variant="body2">Tutorials</Typography>
+            </Link>
+            <Link to={"/add"} className={classes.link}>
+              <Typography variant="body2">Add</Typography>
+            </Link>
+          </Toolbar>
+        </AppBar>
+
+        <Switch>
+          <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
+          <Route exact path="/add" component={AddTutorial} />
+          <Route path="/tutorials/:id" component={Tutorial} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
---
+++ src/components/tutorials-list.component.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class TutorialsList extends Component {
+  render() {
+    return <div></div>;
+  }
+}
